fd1654704d83cf6aa1b61bd8622eb7f5726c20b1
Falaleev Maxim <max@studio107.ru>
2018-01-18 15:50:30 +0300

    fix idea autocompletion

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -3,8 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of Mindy Framework.
- * (c) 2018 Maxim Falaleev
+ * Studio 107 (c) 2018 Maxim Falaleev
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,23 +14,20 @@ namespace Mindy\Bundle\MindyBundle\Controller;
 use Mindy\Bundle\PaginationBundle\Utils\PaginationTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
 
-if (trait_exists(PaginationTrait::class)) {
-    /**
-     * Class Controller
-     *
-     * @deprecated since 4.0
-     */
-    abstract class Controller extends BaseController
-    {
-        use PaginationTrait;
-    }
-} else {
-    /**
-     * Class Controller
-     *
-     * @deprecated since 4.0
-     */
-    abstract class Controller extends BaseController
+if (false === trait_exists(PaginationTrait::class)) {
+    trait PaginationTraitMock
     {
     }
+
+    class_alias(PaginationTraitMock::class, PaginationTrait::class);
+}
+
+/**
+ * Class Controller
+ *
+ * @deprecated since 4.0
+ */
+abstract class Controller extends BaseController
+{
+    use PaginationTrait;
 }
